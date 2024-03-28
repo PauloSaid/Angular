4a01b3850e03e89eb65b4316c890f2b4ad9ddaed
Paulo Said <pauloandreribeirosaid@gmail.com>
2024-03-28 15:11:09 -0300

    v2.0.0 -> New project from scratch. Create of home page and basic routing.

---
+++ MyProject/src/app/Info/features/features.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-features',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      features works!
+    </p>
+  `,
+  styles: ``
+})
+export class FeaturesComponent {
+
+}
---
+++ MyProject/src/app/app.routes.ts
@@ -0,0 +1,13 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './Navigation/home/home.component';
+import { AboutComponent } from './Info/about/about.component';
+import { FeaturesComponent } from './Info/features/features.component';
+import { ContactComponent } from './Info/contact/contact.component';
+
+export const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full'},
+    { path: 'home', component: HomeComponent},
+    { path: 'about', component: AboutComponent},
+    { path: 'contact', component: ContactComponent},
+    { path: 'features', component: FeaturesComponent}
+];
